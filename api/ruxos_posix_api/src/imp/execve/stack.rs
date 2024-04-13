fc0b419f99c78e96b6245a80106f8a28e9041cbc
thesayol <thesayol>
2024-04-13 11:53:11 +0800

    format

--- api/ruxos_posix_api/src/imp/execve/stack.rs
+++ api/ruxos_posix_api/src/imp/execve/stack.rs
@@ -15,17 +15,15 @@ pub struct Stack {
 impl Stack {
     /// alloc a stack
     pub fn new() -> Self {
-        let data = vec![0u8; STACK_SIZE];
-        let top = STACK_SIZE;
-
-        Self { data, top }
+        Self {
+            data: vec![0u8; STACK_SIZE],
+            top: STACK_SIZE,
+        }
     }
 
     /// panic if overflow
     fn panic_if_of(&self) {
-        if self.top > self.data.len() {
-            panic!("sys_execve: stack overflow");
-        }
+        assert!(self.top <= self.data.len(), "sys_execve: stack overflow.");
     }
 
     /// move sp to align
@@ -36,25 +34,23 @@ impl Stack {
 
     /// addr of top of stack
     pub fn sp(&self) -> usize {
-        let start_addr = self.data.as_ptr() as usize;
-        start_addr + self.top
+        self.data.as_ptr() as usize + self.top
     }
 
     /// push data to stack and return the addr of sp
     pub fn push<T>(&mut self, data: &[T], align: usize) -> usize {
         // move sp to right place
-        let size = core::mem::size_of_val(data);
-        self.top -= size;
+        self.top -= core::mem::size_of_val(data);
         self.top = self.align(align);
 
         self.panic_if_of();
 
         // write data into stack
-        let pt = self.sp() as *mut T;
+        let sp = self.sp() as *mut T;
         unsafe {
-            pt.copy_from_nonoverlapping(data.as_ptr(), data.len());
+            sp.copy_from_nonoverlapping(data.as_ptr(), data.len());
         }
 
-        pt as usize
+        sp as usize
     }
 }
